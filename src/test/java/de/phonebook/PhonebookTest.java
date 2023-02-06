47ed4ab5ee5369766d5e6f9d953da4cfd6f060d0
Violeta Abramova <abramova.violetta@gmail.com>
2023-02-06 21:19:16 +0100

    Generate cucumber json report

--- src/test/java/de/phonebook/PhonebookTest.java
+++ src/test/java/de/phonebook/PhonebookTest.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "pretty", "json:target/cucumber-reports" })
+@CucumberOptions(plugin = { "pretty", "json:target/cucumber-reports.json" })
 public class PhonebookTest {
 }
